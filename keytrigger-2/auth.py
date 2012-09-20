cb1ebf1d5654b7e6b0de04a583605169010e7253
rockachu2@gmail.com <rockachu2@gmail.com>
2012-09-20 13:53:20 -0700

    v 0.1, see changelog

--- keytrigger-2/auth.py
+++ keytrigger-2/auth.py
@@ -13,3 +13,30 @@ except Exception, e:
 		sys.exit()
 
 if DEBUG: print "Grabbed Password"
+
+try:
+	saltFile = open(authDir + "salt", 'r')
+	hashFile = open(authDir + "hash", 'r')
+except Exception, e:
+	if DEBUG: print e
+	print "Unable to open password and salt files."
+	sys.exit()
+
+try:
+	salt = saltFile.readline()
+	hash = hashFile.readline()
+
+	if DEBUG: print hash, salt, hashFile, saltFile
+
+	hashObject = hashlib.sha512()
+	hashObject.update(salt)
+	hashObject.update(passwordInput)
+	passwordHash = hashObject.hexdigest()
+
+	if DEBUG: print passwordHash
+
+	if passwordHash == hash:
+		child = subprocess.Popen([sys.executable, "term.py"])
+		if DEBUG: subprocessPrint(child) ## debugtools reference
+except Exception, e:
+	if DEBUG: print e
